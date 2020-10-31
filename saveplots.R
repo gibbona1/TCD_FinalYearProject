cb34f52045b699be159348b74bed4e9387305a1e
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2020-10-31 12:55:56 +0000

    Plots in folder

--- saveplots.R
+++ saveplots.R
@@ -3,7 +3,8 @@ source("covidplots-group.r")
 for(country in names(plotslist)){
   for(p in names(plotslist[[country]])){
     ggsave(filename = paste0(country, "-", gsub("*", "", p, fixed = TRUE), ".png"),
-           plot = plotslist[[country]][[p]])
+           plot = plotslist[[country]][[p]],
+           path = "Plots")
   }
 }
 
