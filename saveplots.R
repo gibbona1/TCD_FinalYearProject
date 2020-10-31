d13ee8ada8a1f70b9fb005b798dca6d6b2bca1d7
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2020-10-31 14:43:32 +0000

    plot size

--- saveplots.R
+++ saveplots.R
@@ -4,7 +4,10 @@ for(country in names(plotslist)){
   for(p in names(plotslist[[country]])){
     ggsave(filename = paste0(country, "-", gsub("*", "", p, fixed = TRUE), ".png"),
            plot = plotslist[[country]][[p]],
-           path = "Plots")
+           path = "Plots",
+           height = 10,
+           width  = 14,
+           units  = "cm"
+           )
   }
 }
-
