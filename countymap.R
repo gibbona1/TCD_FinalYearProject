3e396fe08f3e3d7c162fb50de0c57e5e432a9eb4
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2020-11-24 21:38:27 +0000

    read county shape from URL and unzip
    Title and subtitle change

--- countymap.R
+++ countymap.R
@@ -7,7 +7,13 @@ library(dplyr)
 countyplotlist <- list()
 
 # read the shape file
-countyshp <- readOGR("C:/Users/Anthony/Downloads/counties/counties.shp")
+temp  <- tempfile()
+temp2 <- tempfile()
+
+download.file("https://www.townlands.ie/static/downloads/counties.zip", temp)
+unzip(temp, exdir = temp2)
+shape_dir <- paste(temp2, list.files(temp2, pattern = "shp"), sep = "\\")
+countyshp <- readOGR(shape_dir)
 
 # read the county case data
 countycases <- read.csv("https://opendata-geohive.hub.arcgis.com/datasets/4779c505c43c40da9101ce53f34bb923_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D")
@@ -35,8 +41,8 @@ countyplotlist[["rep"]] <- ggplot(countycase_map) +
   geom_polygon(colour="grey40") + labs(fill = "Cases per 100k") +
   scale_fill_gradientn(colours = col_grad) +
   geom_text(data = latest_dat, aes(x = Long, y = Lat, label = floor(PopulationProportionCovidCases)), inherit.aes = FALSE) +
-  ggtitle("Covid cases per 100,000 population by county", 
-          subtitle = paste("Date:", format.Date(latest_date, "%B %d, %Y"))) +
+  ggtitle("Cumulative cases per 100,000 population by county", 
+          subtitle = paste("Up to", format.Date(latest_date, "%B %d, %Y"))) +
   theme(axis.title       = element_blank(),
         axis.text        = element_blank(),
         axis.ticks       = element_blank(),
@@ -47,8 +53,8 @@ countyplotlist[["names"]] <- ggplot(countycase_map) +
   geom_polygon(colour="grey40") + labs(fill = "Cases per 100k") +
   scale_fill_gradientn(colours = col_grad) +
   geom_text(data = latest_dat, aes(x = Long, y = Lat, label = CountyName), size=3,inherit.aes = FALSE) +
-  ggtitle("Covid cases per 100,000 population by county", 
-          subtitle = paste("Date:", format.Date(latest_date, "%B %d, %Y"))) +
+  ggtitle("Cumulative cases per 100,000 population by county", 
+          subtitle = paste("Up to", format.Date(latest_date, "%B %d, %Y"))) +
   theme(axis.title       = element_blank(),
         axis.text        = element_blank(),
         axis.ticks       = element_blank(),
@@ -58,8 +64,8 @@ countyplotlist[["blank"]] <- ggplot(countycase_map) +
   aes(long, lat, group=group, fill=PopulationProportionCovidCases) +
   geom_polygon(colour="grey40") + labs(fill = "Cases per 100k") +
   scale_fill_gradientn(colours = col_grad) +
-  ggtitle("Covid cases per 100,000 population by county", 
-          subtitle = paste("Date:", format.Date(latest_date, "%B %d, %Y"))) +
+  ggtitle("Cumulative cases per 100,000 population by county", 
+          subtitle = paste("Up to", format.Date(latest_date, "%B %d, %Y"))) +
   theme(axis.title       = element_blank(),
         axis.text        = element_blank(),
         axis.ticks       = element_blank(),
